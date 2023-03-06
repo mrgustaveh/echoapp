e2789616413d9f5f751aed47c7eaa37d12a1d163
mburuanthony <mburuantony@outlook.com>
2023-03-06 12:12:43 +0300

    setup firebase config

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtIB7jG-8vUhsvgjNxvWDkRajYKoFrdp0",
+  authDomain: "echo-9f6ea.firebaseapp.com",
+  projectId: "echo-9f6ea",
+  storageBucket: "echo-9f6ea.appspot.com",
+  messagingSenderId: "1064311130616",
+  appId: "1:1064311130616:web:546e59173c882fe6b62e0d",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
