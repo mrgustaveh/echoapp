f0daf5e8892dd425c433bf7d4840fefaa2d465e0
mburuanthony <mburuantony@outlook.com>
2023-03-23 16:27:40 +0300

    craeted styles

---
+++ constants/styles.js
@@ -0,0 +1,56 @@
+import { colors } from "./colors";
+
+export const container = {
+  flex: 1,
+  backgroundColor: colors.darkbg,
+};
+
+export const text = {
+  fontSize: 16,
+  fontWeight: "500",
+  color: colors.text,
+};
+
+export const subtitle = {
+  fontSize: 18,
+  fontWeight: "600",
+  color: colors.text,
+};
+
+export const title = {
+  fontSize: 20,
+  fontWeight: "700",
+  color: colors.text,
+};
+
+export const actions = {
+  width: "100%",
+  height: "100%",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "flex-end",
+};
+
+export const downloadbtn = {
+  width: 40,
+  height: 40,
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: 4,
+  marginHorizontal: 16,
+  borderRadius: 32,
+  backgroundColor: colors.active,
+};
+
+export const deletebtn = {
+  padding: 8,
+  marginTop: 32,
+  marginHorizontal: 8,
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  borderWidth: 1,
+  borderRadius: 6,
+  borderColor: colors.gray,
+};
