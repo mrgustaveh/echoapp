a93f8928f80ecf33bae6a23ed9ad8e908a7f1983
mburuanthony <mburuantony@outlook.com>
2023-05-05 16:46:27 +0300

    created homescreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import { NavBar } from "../components/NavBar";
+import { container, title } from "../constants/styles";
+import { BottomBtn } from "../components/buttons/BottomBtn";
+import AntDeisgnIcon from "react-native-vector-icons/AntDesign";
+import { colors } from "../constants/colors";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={container}>
+      <NavBar screen="home" />
+
+      <BottomBtn
+        title="create"
+        icon={<AntDeisgnIcon name="plus" color={colors.text} size={22} />}
+        onclick={() => navigation.navigate("detail")}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
