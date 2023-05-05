9b03d338fb25911dfc64a3435a7887e80b1404a9
mburuanthony <mburuantony@outlook.com>
2023-05-05 16:45:21 +0300

    added screens to stack

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./screens/SplashScreen";
 import Authentication from "./screens/Authentication";
+import HomeScreen from "./screens/HomeScreen";
+import SearchScreen from "./screens/SearchScreen";
+import CreateAudioScreen from "./screens/CreateAudioScreen";
+import DetailScreen from "./screens/DetailScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
@@ -19,6 +23,10 @@ export default function App() {
             animationTypeForReplace: "push",
           }}
         >
+          <Stack.Screen name="home" component={HomeScreen} />
+          <Stack.Screen name="search" component={SearchScreen} />
+          <Stack.Screen name="create" component={CreateAudioScreen} />
+          <Stack.Screen name="detail" component={DetailScreen} />
           <Stack.Screen name="splash" component={SplashScreen} />
           <Stack.Screen name="authentication" component={Authentication} />
           <Stack.Screen name="profile" component={ProfileScreen} />
