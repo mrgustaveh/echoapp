288691134ed6f4bf1f59d9311d6dc3cf9b65a2b7
mburuanthony <mburuantony@outlook.com>
2023-05-05 16:46:06 +0300

    creatd button component

---
+++ components/buttons/BottomBtn.jsx
@@ -0,0 +1,37 @@
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { subtitle } from "../../constants/styles";
+import { colors } from "../../constants/colors";
+
+export const BottomBtn = ({ title, icon, onclick, iconfirst }) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        { flexDirection: iconfirst ? "row-reverse" : "row" },
+      ]}
+      onPress={onclick}
+    >
+      <Text style={[subtitle, styles.title]}>{title}</Text>
+      {icon}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 16,
+    width: "90%",
+    alignSelf: "center",
+    marginHorizontal: 8,
+    paddingVertical: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    borderRadius: 32,
+    backgroundColor: colors.accent,
+  },
+  title: {
+    textTransform: "capitalize",
+  },
+});
