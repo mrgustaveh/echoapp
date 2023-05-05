c403a48a763789f56c5205673aa2ec538dd49c54
mburuanthony <mburuantony@outlook.com>
2023-05-05 16:46:46 +0300

    cretaed search screen

---
+++ screens/SearchScreen.js
@@ -0,0 +1,27 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import EntypoIcon from "react-native-vector-icons/Entypo";
+import { BottomBtn } from "../components/buttons/BottomBtn";
+import { container } from "../constants/styles";
+import { colors } from "../constants/colors";
+
+const SearchScreen = () => {
+  const navigation = useNavigation();
+
+  const goback = () => navigation.goBack();
+
+  return (
+    <SafeAreaView style={container}>
+      <BottomBtn
+        title="go back"
+        icon={
+          <EntypoIcon name="chevron-thin-left" size={22} color={colors.text} />
+        }
+        iconfirst
+        onclick={goback}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default SearchScreen;
