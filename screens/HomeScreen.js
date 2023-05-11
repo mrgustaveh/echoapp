05aa3847d6b2e2949e659b7390287e907d7465bc
mburuanthony <mburuantony@outlook.com>
2023-05-11 14:32:05 +0300

    modified home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
+import AntDeisgnIcon from "react-native-vector-icons/AntDesign";
 import { NavBar } from "../components/NavBar";
 import { container, title } from "../constants/styles";
 import { BottomBtn } from "../components/buttons/BottomBtn";
-import AntDeisgnIcon from "react-native-vector-icons/AntDesign";
+import { PreviewCtr } from "../components/home/PreviewCtr";
 import { colors } from "../constants/colors";
 
 const HomeScreen = () => {
@@ -11,9 +13,20 @@ const HomeScreen = () => {
 
   const gotocreate = () => navigation.navigate("create");
 
+  const data = [1, 2, 3, 4, 5];
+
   return (
     <SafeAreaView style={container}>
       <NavBar screen="home" />
+
+      <FlatList
+        style={{ alignSelf: "stretch", paddingHorizontal: 7 }}
+        data={data}
+        keyExtractor={(item) => item}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        renderItem={({ item }) => <PreviewCtr />}
+      />
 
       <BottomBtn
         title="create"
