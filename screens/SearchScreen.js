dd6595c8057603252e5985bacddba73143a1bf87
mburuanthony <mburuantony@outlook.com>
2023-05-11 14:32:38 +0300

    modified search goback icon

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,6 +2,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import EntypoIcon from "react-native-vector-icons/Entypo";
 import { BottomBtn } from "../components/buttons/BottomBtn";
+import { SearchInput } from "../components/search/SearchInput";
 import { container } from "../constants/styles";
 import { colors } from "../constants/colors";
 
@@ -12,11 +13,11 @@ const SearchScreen = () => {
 
   return (
     <SafeAreaView style={container}>
+      <SearchInput />
+
       <BottomBtn
         title="go back"
-        icon={
-          <EntypoIcon name="chevron-thin-left" size={22} color={colors.text} />
-        }
+        icon={<EntypoIcon name="chevron-left" size={22} color={colors.text} />}
         iconfirst
         onclick={goback}
       />
