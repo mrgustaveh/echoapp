1729dd5106526fc1328623704af3f89b1e05e95a
mburuanthony <mburuantony@outlook.com>
2023-05-14 23:48:54 +0300

    created subs component

---
+++ components/profile/Subscriptions.jsx
@@ -0,0 +1,114 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Divider } from "../global/Divider";
+import { ClipboardIcon, CheckIcon } from "../../assets/icons/icons";
+import { text } from "../../constants/styles";
+import { colors } from "../../constants/colors";
+
+export const Subscriptions = ({ currplan }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.plan}>
+        <View style={styles.miscctr}>
+          <View style={styles.clipbrdctr}>
+            <ClipboardIcon />
+          </View>
+
+          <View style={styles.plandescription}>
+            <Text style={text}>Basic Plan</Text>
+            <Text style={text}>$3 / 1,024 characters</Text>
+          </View>
+
+          {currplan === "basic" ? <CheckIcon /> : <View />}
+        </View>
+
+        <TouchableOpacity style={styles.pressable}>
+          <Text style={text}>
+            {currplan === "basic" ? "Buy Again" : "Buy Plan"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Divider />
+
+      <View style={styles.plan}>
+        <View style={styles.miscctr}>
+          <View style={styles.clipbrdctr}>
+            <ClipboardIcon />
+          </View>
+
+          <View style={styles.plandescription}>
+            <Text style={text}>Standard Plan</Text>
+            <Text style={text}>$5 / 5,000 characters</Text>
+          </View>
+
+          {currplan === "standard" ? <CheckIcon /> : <View />}
+        </View>
+
+        <TouchableOpacity style={styles.pressable}>
+          <Text style={text}>
+            {currplan === "standard" ? "Buy Again" : "Buy Plan"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Divider />
+
+      <View style={styles.plan}>
+        <View style={styles.miscctr}>
+          <View style={styles.clipbrdctr}>
+            <ClipboardIcon />
+          </View>
+
+          <View style={styles.plandescription}>
+            <Text style={text}>Premium Plan</Text>
+            <Text style={text}>$10 / 10,024 characters</Text>
+          </View>
+
+          {currplan === "premium" ? <CheckIcon /> : <View />}
+        </View>
+
+        <TouchableOpacity style={styles.pressable}>
+          <Text style={text}>
+            {currplan === "premium" ? "Buy Again" : "Buy Plan"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    marginHorizontal: 16,
+    padding: 8,
+    borderRadius: 12,
+    backgroundColor: colors.accent,
+  },
+  plan: {
+    marginVertical: 8,
+  },
+  miscctr: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  clipbrdctr: {
+    width: 60,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 60,
+    backgroundColor: colors.primarylight,
+  },
+  plandescription: {
+    gap: 6,
+  },
+  pressable: {
+    marginTop: 8,
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 6,
+    backgroundColor: colors.primarylight,
+  },
+});
