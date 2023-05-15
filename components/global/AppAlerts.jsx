6061a88422651be84e680aa464c530a515b96c2f
mburuanthony <mburuantony@outlook.com>
2023-05-15 22:14:46 +0300

    created alert component

---
+++ components/global/AppAlerts.jsx
@@ -0,0 +1,54 @@
+import { StyleSheet, View } from "react-native";
+import Lottie from "lottie-react-native";
+import { usealert } from "../../context/alertctx";
+import { colors } from "../../constants/colors";
+
+export const AppAlert = () => {
+  const { isloading, issuccess } = usealert();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.animations}>
+        {isloading && (
+          <Lottie
+            source={require("../../assets/animations/loading.json")}
+            autoPlay
+            loop
+          />
+        )}
+
+        {!isloading && issuccess && (
+          <Lottie
+            source={require("../../assets/animations/success.json")}
+            autoPlay
+            loop={false}
+          />
+        )}
+
+        {!isloading && !issuccess && (
+          <Lottie
+            source={require("../../assets/animations/error.json")}
+            autoPlay
+            loop={false}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.primarylight,
+  },
+  animations: {
+    width: "94%",
+    height: "30%",
+    borderRadius: 12,
+  },
+});
