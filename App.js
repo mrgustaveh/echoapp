48c74efb0c78f9ed55a6c7d31375009950611585
mburuanthony <mburuantony@outlook.com>
2023-05-15 22:15:24 +0300

    modified app

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import "expo-dev-client";
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { AuthProvider } from "./context/authctxt";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createaccount, getuser } from "./utils/api/auth";
+import { AuthProvider, useAuth } from "./context/authctxt";
+import { AlertProvider, usealert } from "./context/alertctx";
+import { AppAlert } from "./components/global/AppAlerts";
 import Authentication from "./screens/Authentication";
 import HomeScreen from "./screens/HomeScreen";
 import SearchScreen from "./screens/SearchScreen";
@@ -13,6 +19,46 @@ import ProfileScreen from "./screens/ProfileScreen";
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const { setisvible, isvisible, setisloading, setissuccess } = usealert();
+  const { authenticated, idToken, setUserUid } = useAuth();
+  const [accountchecked, setaccountchecked] = useState(false);
+
+  useEffect(() => {
+    AsyncStorage.getItem("prevauth").then((res) => {
+      const data = JSON.parse(res);
+
+      if (data?.prevauth) {
+        setisvible(true);
+        setisloading(true);
+      } else {
+        setisvible(false);
+      }
+    });
+    // console.log(idToken);
+    if (authenticated && idToken !== "") {
+      getuser({ idtoken: idToken })
+        .then(async (res) => {
+          if (res.user?.length === 0) {
+            const { user } = await createaccount({ idtoken: idToken });
+            setUserUid(user[0]?.user_uid);
+          } else {
+            setUserUid(res[0]?.user_uid);
+          }
+
+          setaccountchecked(true);
+          setisvible(false);
+        })
+        .catch(() => {
+          setisloading(false);
+          setissuccess(false);
+
+          setTimeout(() => {
+            setisvible(false);
+          }, 3500);
+        });
+    }
+  }, [authenticated, idToken]);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
@@ -23,18 +69,26 @@ function App() {
             animationTypeForReplace: "push",
           }}
         >
-          <Stack.Screen name="authentication" component={Authentication} />
-          <Stack.Screen name="home" component={HomeScreen} />
-          <Stack.Screen name="search" component={SearchScreen} />
-          <Stack.Screen name="create" component={CreateAudioScreen} />
-          <Stack.Screen name="detail" component={DetailScreen} />
-          <Stack.Screen name="profile" component={ProfileScreen} />
+          {authenticated && idToken !== "" && accountchecked ? (
+            <>
+              <Stack.Screen name="home" component={HomeScreen} />
+              <Stack.Screen name="search" component={SearchScreen} />
+              <Stack.Screen name="create" component={CreateAudioScreen} />
+              <Stack.Screen name="detail" component={DetailScreen} />
+              <Stack.Screen name="profile" component={ProfileScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="authentication" component={Authentication} />
+            </>
+          )}
         </Stack.Navigator>
         <StatusBar
           style="light"
           animated={true}
           backgroundColor="transparent"
         />
+        {isvisible && <AppAlert />}
       </NavigationContainer>
     </SafeAreaProvider>
   );
@@ -43,7 +97,9 @@ function App() {
 export default function AppProvider() {
   return (
     <AuthProvider>
-      <App />
+      <AlertProvider>
+        <App />
+      </AlertProvider>
     </AuthProvider>
   );
 }
