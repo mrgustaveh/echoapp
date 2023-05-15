c3bff153caa2438855c4c87123f5ec9c46d01fc1
mburuanthony <mburuantony@outlook.com>
2023-05-15 22:15:02 +0300

    modified auth context

--- context/authctxt.js
+++ context/authctxt.js
@@ -4,7 +4,6 @@ import { auth } from "../firebase/config";
 
 const authcontext = createContext({
   authenticated: false,
-  setauthenticated: () => {},
   idToken: "",
   userUid: "",
   setUserUid: () => {},
