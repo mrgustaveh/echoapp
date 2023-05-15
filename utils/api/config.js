3a6678c2a30b4a677640e4f6b5ff8ce082a28a48
mburuanthony <mburuantony@outlook.com>
2023-05-15 09:40:53 +0300

    api config

---
+++ utils/api/config.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://127.0.0.1:8000/";
+
+export const authurls = {
+  me: "account/me/",
+  destroy: "account/destroy/",
+};
+
+export const prompturls = {
+  prompts: "app/prompts/",
+};
