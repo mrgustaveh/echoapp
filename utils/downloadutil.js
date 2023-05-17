40e81338b0d76b99d92482a1c1eb6b50909e889a
mburuanthony <mburuantony@outlook.com>
2023-05-17 23:42:26 +0300

    minor update

--- utils/downloadutil.js
+++ utils/downloadutil.js
@@ -2,14 +2,19 @@ import * as FileSystem from "expo-file-system";
 import * as MediaLibrary from "expo-media-library";
 
 export async function downloadmedia({ URL, title }) {
+  let filesaved = false;
+
   const downloadresumable = FileSystem.createDownloadResumable(
     URL,
-    FileSystem.documentDirectory + `${title}.mpeg`,
+    FileSystem.documentDirectory + `${title}.mp3`,
     {}
   );
 
   try {
     const { uri } = await downloadresumable.downloadAsync();
     await MediaLibrary.saveToLibraryAsync(uri);
+    filesaved = true;
+
+    return { filesaved };
   } catch (e) {}
 }
