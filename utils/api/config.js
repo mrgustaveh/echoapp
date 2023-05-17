a147811db4f650013f44dffeb8d25804fd75800d
mburuanthony <mburuantony@outlook.com>
2023-05-17 23:42:30 +0300

    minor update

--- utils/api/config.js
+++ utils/api/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://b694-154-159-237-126.ngrok-free.app/";
+export const BASE_URL = "https://44aa-154-159-237-53.ngrok-free.app/";
+// https://44aa-154-159-237-53.ngrok-free.app
 
 export const authurls = {
   me: "account/me/",
