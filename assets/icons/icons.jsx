43e2ed3c0e79d8c7a61ea5deeef4041a5ffd21a9
mburuanthony <mburuantony@outlook.com>
2023-06-01 10:07:03 +0300

    fixed trash icon width & height

--- assets/icons/icons.jsx
+++ assets/icons/icons.jsx
@@ -6,8 +6,8 @@ const { text } = colors;
 export const TrashIcon = () => {
   return (
     <Svg
-      width="22"
-      height="24"
+      width="16"
+      height="18"
       viewBox="0 0 22 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
