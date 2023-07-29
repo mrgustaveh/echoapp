d08faa199ad3cdc7ad01cb4aeacc5f29639ed7f3
mburuanthony <mburuantony@outlook.com>
2023-07-29 10:56:19 +0300

    modified colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,12 +1,8 @@
 export const colors = {
-  primary: "rgba(25, 30, 35, 1)",
-  primarylight: "rgba(25, 30, 35, 0.5)",
-  accent: "rgba(56, 60, 72, 1)",
-  accentlight: "rgba(56, 60, 72, 0.55)",
-  royalblue: "rgba(72, 106, 218, 1)",
+  primary: "rgba(38, 38, 38, 1)",
+  accent: "rgba(50, 50, 50, 1)",
+  text: "rgba(127, 127, 127, 1)",
+  lineclr: "rgba(248, 250, 252, 0.2)",
   fbblue: "rgba(60, 84, 164, 0.6)",
   red: "rgba(209, 0, 0, 1)",
-  text: "rgba(218, 218, 225, 1)",
-  textlight: "rgba(218, 218, 225, 0.6)",
-  blue: "rgba(16, 14, 29, 0.8)",
 };
