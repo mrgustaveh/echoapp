6b9bba47f9b56d9ba864ed9b8d0c813859932fe4
mburuanthony <mburuantony@outlook.com>
2023-07-29 14:16:57 +0300

    minor fix

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,9 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, TouchableOpacity, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
-import { BottomBtn } from "../components/buttons/BottomBtn";
-import EntypoIcon from "react-native-vector-icons/Entypo";
 import { Subscriptions } from "../components/profile/Subscriptions";
 import { auth } from "../firebase/config";
+import { ChevronIcon, SignOutIcon } from "../assets/icons/icons";
 import { container, subtitle, text } from "../constants/styles";
 import { colors } from "../constants/colors";
 
@@ -26,18 +25,19 @@ function ProfileScreen() {
         <Text style={subtitle}>{auth?.currentUser?.email}</Text>
       </View>
 
-      <Subscriptions currplan="basic" />
+      <Subscriptions currplan="premium" />
 
-      <Pressable onPress={onsignout}>
-        <Text style={[text, styles.logout]}>Log Out</Text>
-      </Pressable>
+      <View style={styles.actions}>
+        <TouchableOpacity style={[styles.container]} onPress={goback}>
+          <ChevronIcon />
+          <Text style={[text, styles.title]}>go back</Text>
+        </TouchableOpacity>
 
-      <BottomBtn
-        title="go back"
-        icon={<EntypoIcon name="chevron-left" size={22} color={colors.text} />}
-        iconfirst
-        onclick={goback}
-      />
+        <TouchableOpacity onPress={onsignout} style={styles.signout}>
+          <SignOutIcon />
+          <Text style={text}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -55,10 +55,41 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     resizeMode: "cover",
   },
-  logout: {
-    marginVertical: 16,
-    marginHorizontal: 16,
-    color: colors.red,
+  signout: {
+    width: "32%",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 16,
+    backgroundColor: colors.accent,
+    borderWidth: 0.5,
+    borderRadius: 6,
+    borderColor: colors.lineclr,
+  },
+  container: {
+    width: "60%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    padding: 10,
+    gap: 12,
+    borderWidth: 0.5,
+    borderRadius: 6,
+    borderColor: colors.lineclr,
+    backgroundColor: colors.accent,
+  },
+  title: {
+    textTransform: "capitalize",
+  },
+  actions: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 8,
+    position: "absolute",
+    bottom: 8,
   },
 });
 
