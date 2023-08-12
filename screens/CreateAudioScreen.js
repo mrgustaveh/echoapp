e41a9756b5117dee93b727ee4509e4d02f7481a4
mburuanthony <mburuantony@outlook.com>
2023-08-12 12:22:30 +0300

    minor fix

--- screens/CreateAudioScreen.js
+++ screens/CreateAudioScreen.js
@@ -1,34 +1,140 @@
-import { useState } from "react";
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Text, TextInput, Keyboard } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
 import { NavBar } from "../components/NavBar";
 import { BottomBtn } from "../components/buttons/BottomBtn";
 import { CreateIcon } from "../assets/icons/icons";
 import { Divider } from "../components/global/Divider";
 import { VoicePreview } from "../components/create/VoicePreview";
-import { container, subtitle, text } from "../constants/styles";
+import { useAuth } from "../context/authctxt";
+import { usenotification } from "../context/notificationctx";
+import { createprompt } from "../utils/api/prompts";
 import { colors } from "../constants/colors";
+import { container, subtitle, text } from "../constants/styles";
 
 const CreateAudioScreen = () => {
   const [audiotitle, setaudiotitle] = useState("");
   const [audiocontent, setaudiocontent] = useState("");
+  const [keybrdisvsble, setkeybrdisvsble] = useState(false);
+  const [voiceId, setVoiceid] = useState("EXAVITQu4vr4xnSDxMaL");
+
+  const navigation = useNavigation();
+  const { idToken } = useAuth();
+  const {
+    setshownotification,
+    setnotifiIsloading,
+    setissuccess,
+    setnotificationtitle,
+    setnotificationtext,
+  } = usenotification();
 
   const { red, text } = colors;
-  const CONTENT_THRESH = 32;
+
+  const CONTENT_THRESH = 200;
+
+  const emoji_regex =
+    /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
+
+  const audiosamples = [
+    {
+      voiceid: "ErXwobaYiN019PkySvjV",
+      title: "Antoni",
+      previmg: "",
+      audioUrl: "",
+    },
+    {
+      voiceid: "EXAVITQu4vr4xnSDxMaL",
+      title: "Bella",
+      previmg: "",
+      audioUrl: "",
+    },
+    {
+      voiceid: "XB0fDUnXU5powFXDhCwa",
+      title: "Charlotte",
+      previmg: "",
+      audioUrl: "",
+    },
+    {
+      voiceid: "ThT5KcBeYPX3keUQqHPh",
+      title: "Dorothy",
+      previmg: "",
+      audioUrl: "",
+    },
+    {
+      voiceid: "zcAOhNBS3c14rBihAFp1",
+      title: "Giovanni",
+      previmg: "",
+      audioUrl: "",
+    },
+    {
+      voiceid: "XrExE9yKIg1WjnnlVkGX",
+      title: "Matilda",
+      previmg: "",
+      audioUrl: "",
+    },
+    {
+      voiceid: "onwK4e9ZLuTAKqWW03F9",
+      title: "Daniel",
+      previmg: "",
+      audioUrl: "",
+    },
+  ];
 
   const titleerror = () => {
-    return audiotitle.length > 22 || audiotitle.includes(" ") ? true : false;
+    return audiotitle.length > 22 ||
+      emoji_regex.test(audiotitle) ||
+      audiotitle.includes(" ")
+      ? true
+      : false;
   };
 
   const contenterror = () => {
-    const emoji_regex =
-      /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
-
     return audiocontent.length > CONTENT_THRESH ||
       emoji_regex.test(audiocontent)
       ? true
       : false;
   };
+
+  const oncreateprompt = async () => {
+    setshownotification(true);
+    setnotifiIsloading(true);
+    setnotificationtitle("processing");
+    setnotificationtext("processing your prompt...");
+
+    const { isok, prompt } = await createprompt({
+      idtoken: idToken,
+      ptitle: audiotitle,
+      ptext: audiocontent,
+      pvid: voiceId,
+    });
+
+    if (isok) {
+      setshownotification(true);
+      setnotifiIsloading(false);
+      setissuccess(true);
+      setnotificationtitle("success");
+      setnotificationtext("your prompt was processed successfully");
+
+      navigation.navigate("detail", { promptID: prompt?.promptUid });
+    } else {
+      setshownotification(true);
+      setnotifiIsloading(false);
+      setissuccess(false);
+      setnotificationtitle("error");
+      setnotificationtext("we were unable to process your prompt");
+    }
+  };
+
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", () => {
+      setkeybrdisvsble(true);
+    });
+
+    Keyboard.addListener("keyboardDidHide", () => {
+      setkeybrdisvsble(false);
+    });
+  }, [keybrdisvsble]);
 
   return (
     <SafeAreaView style={container}>
@@ -96,29 +202,16 @@ const CreateAudioScreen = () => {
           <VoicePreview />
           <VoicePreview isactive />
         </View>
-        {/* <FlatList
-          style={{ alignSelf: "stretch", paddingHorizontal: 7 }}
-          data={myprompts}
-          keyExtractor={(item) => item?.promptUid}
-          showsVerticalScrollIndicator={false}
-          numColumns={2}
-          renderItem={({ item }) => (
-            <VoicePreview
-              voiceid={item?.promptUid}
-              description={item?.prompt}
-              audioUrl={item?.audio[0]?.audio?.audio}
-              isactive={{selectedidx === thisidx ? true : false}}
-            />
-          )}
-        />  */}
       </View>
 
-      <BottomBtn
-        title="create"
-        icon={<CreateIcon />}
-        btnDisabled={titleerror() || contenterror()}
-        onclick={() => {}}
-      />
+      {!keybrdisvsble && (
+        <BottomBtn
+          title="create"
+          icon={<CreateIcon />}
+          btnDisabled={titleerror() || contenterror()}
+          onclick={oncreateprompt}
+        />
+      )}
     </SafeAreaView>
   );
 };
