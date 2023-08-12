eeef37772df9fd5947512b98f342a9dadc6dbc06
mburuanthony <mburuantony@outlook.com>
2023-08-12 12:21:19 +0300

    updated preview U

--- components/home/PreviewCtr.jsx
+++ components/home/PreviewCtr.jsx
@@ -1,11 +1,9 @@
-import { StyleSheet, Dimensions, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useplayer } from "../../context/playerctx";
 import { PlayIcon } from "../../assets/icons/icons";
 import { colors } from "../../constants/colors";
 import { text } from "../../constants/styles";
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export const PreviewCtr = ({ promptUid, description, audioUrl, title }) => {
   const navigation = useNavigation();
@@ -16,37 +14,54 @@ export const PreviewCtr = ({ promptUid, description, audioUrl, title }) => {
     navigation.navigate("detail", { promptID: promptUid });
 
   const playaudio = () => {
-    setplyrisvisible(true);
     setaudtitle(title);
     setaudURL(audioUrl);
+    setplyrisvisible(true);
   };
 
   return (
     <TouchableOpacity onPress={gotodetail} style={styles.container}>
       <Text
-        style={[text, { textAlign: "justify", textTransform: "lowercase" }]}
+        style={[
+          text,
+          {
+            textAlignVertical: "center",
+            marginLeft: 8,
+          },
+        ]}
       >
-        {String(description).substring(0, 59)} . . .
+        {String(description).substring(0, 48)}. . .
       </Text>
 
-      <TouchableOpacity onPress={playaudio}>
-        <PlayIcon />
-      </TouchableOpacity>
+      <View style={styles.playctr}>
+        <TouchableOpacity style={styles.playaudio} onPress={playaudio}>
+          <PlayIcon />
+        </TouchableOpacity>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: SCREEN_WIDTH / 2 - 12,
-    height: SCREEN_HEIGHT / 5 - 12,
+    width: "98%",
+    height: 58,
     marginBottom: 8,
-    marginHorizontal: 5,
-    padding: 8,
+    marginHorizontal: 4,
+    flexDirection: "row",
     justifyContent: "space-between",
     borderWidth: 0.5,
     borderColor: colors.lineclr,
-    borderRadius: 6,
+    borderRadius: 4,
     backgroundColor: colors.accent,
+  },
+  playctr: {
+    height: "100%",
+    justifyContent: "center",
+    borderLeftWidth: 0.5,
+    borderColor: colors.lineclr,
+  },
+  playaudio: {
+    margin: 16,
   },
 });
